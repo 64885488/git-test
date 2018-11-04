bd711183b7da5affb2fa31c0177c7e38063f7b37
heima <64885488@qq.com>
2018-11-04 21:45:28 +0800

    把本地工程添加到git

---
+++ git_test/src/com/itheima/test/test.java
@@ -0,0 +1,24 @@
+package com.itheima.test;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+
+    public void test(){
+        int i = 0;
+        i += 10;
+    }
+}
